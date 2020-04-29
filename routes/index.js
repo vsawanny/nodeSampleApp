895477b313af6c2d12db7c3aecefe0c306cb6b6a
vsawanny <46169590+vsawanny@users.noreply.github.com>
2020-04-30 01:39:25 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Prod Express version 1' });
+  res.render('index', { title: 'Prod Express version 2' });
 });
 
 module.exports = router;
